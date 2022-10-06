e36a1fa73200f70393eaa7837ff6a4a158883cec
tmprova <applocker81@gmail.com>
2022-10-06 23:18:37 +0600

    axios

--- src/components/Pages/Home/HomeStore/HomeItem.js
+++ src/components/Pages/Home/HomeStore/HomeItem.js
@@ -90,7 +90,7 @@ const HomeItem = ({ item }) => {
           className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 active:bg-blue-700 disabled:opacity-50 mt-4 w-full flex items-center justify-center"
         >
           Update
-          <svg
+          {/* <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-6 w-6 ml-2"
             fill="none"
@@ -103,7 +103,7 @@ const HomeItem = ({ item }) => {
               strokeWidth="2"
               d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
             />
-          </svg>
+          </svg> */}
         </button>
       </div>
     </div>
--- src/components/Pages/ItemDetails/Itemdetails.js
+++ src/components/Pages/ItemDetails/Itemdetails.js
@@ -1,19 +1,80 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Loading from "../../Loading/Loading";
 
 const Itemdetails = () => {
   const { itemid } = useParams();
   // console.log(params);
   const [items, setItems] = useState({});
+  const [loading, setLoading] = useState(false);
+  const {
+    _id,
+    Name,
+    IMG,
+    Manufacturer,
+    Production,
+    Assembly,
+    Class,
+    Engine,
+    Power,
+    Transmission,
+    Brakes,
+    Price,
+    Quatity,
+    Weight,
+    Torque,
+  } = items;
 
-  // useEffect(() => {
-  //   const url = `InventoryData${itemid}`;
-  //   fetch(url)
-  //     .then((res) => res.json())
-  //     .then((data) => console.log(data));
-  // }, [itemid]);
+  useEffect(() => {
+    const getInventories = async () => {
+      const url = `http://localhost:5000/inventory/${itemid}`;
+      const { data } = await axios.get(url);
 
-  return <div>name:{itemid}</div>;
+      setItems(data);
+      setLoading(false);
+    };
+    getInventories();
+  }, [items, itemid]);
+
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2">
+          <div className="h-full grid grid-cols-1 md:grid-cols-2 justify-items-center gap-4 md:justify-items-around content-center">
+            <div className="order-2 md:order-1">
+              <h1 className="text-3xl">General Info:</h1>
+              <hr />
+              <h1>Bike Name: {Name}</h1>
+              <h1>In Storage: {Quatity}</h1>
+              <h1>Origin: {Assembly ? Assembly : "Not Available"}</h1>
+              <h1>Bike Class: {Class}</h1>
+              <h1>Engine: {Engine}</h1>
+              <h1>Power: {Power}</h1>
+              <h1>Torque: {Torque}</h1>
+              <h1>Brakes: {Brakes}</h1>
+              <h1>Transmission: {Transmission}</h1>
+              <h1>Weight: {Weight}</h1>
+              <h1>Price: {Price}</h1>
+              <h1>Production: {Production}</h1>
+              <h1>Manufacturer: {Manufacturer}</h1>
+              <h1 className="text-3xl mt-4">Scores:</h1>
+              <hr />
+              <h1>Community Score: {Transmission}</h1>
+              <h1>Developer Score: {Price}</h1>
+              <h1>Liquidity Score: {Production}</h1>
+              <h1>Public Interest Score: {Manufacturer}</h1>
+            </div>
+            <div className="flex order-1 md:order-2 justify-center items-center">
+              <img src={IMG ? IMG : "not available"} alt="bikeImg" />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Itemdetails;
